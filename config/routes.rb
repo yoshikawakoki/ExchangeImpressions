cb7c60ada30f77f3b01204f9951924059b9fe78e
yoshikawakoki <cooler970708@gmail.com>
2021-12-10 02:33:43 +0000

    ルーティングの記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
+  #ユーザー
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #トップページ
+  root to: "homes#top"
+  
+  #ユーザー
+  resources users, only: [:index, :show, :edit, :update] do
+    #フォロー、フォロワー
+    resources relationships, only: [:create, :destroy]
+    #フォロー一覧
+    get "followings" => "relationships#followings", as: "followings"
+    #フォロワー一覧
+    get "followers" => "relationships#followers", as: "followers"
+  end
+  
+  #投稿
+  resources posts do
+    #コメント
+    resources post_comments, only: [:create]
+    #いいね
+    resources favorites, only: [:create, :destroy]
+  end
+  
+  #ジャンル
+  resources genres, only: [:index, :create, :edit, :update, :destroy]
+  
+  
 end
