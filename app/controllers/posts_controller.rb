267993843bc273cc9961f12e3c72d56ac259be7c
yoshikawakoki <cooler970708@gmail.com>
2021-12-18 10:46:38 +0000

    投稿機能作成途中

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,26 @@
 class PostsController < ApplicationController
-  
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    @post =Post.new(post_params)
+    @post.save
+  end
+
   def hashtag
     @user = current_user
     @tag = Hashtag.find_by(hashname: params[:name])
     @posts = @tag.posts
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :body, :evaluation, :hashtag_id)
   end
 end
