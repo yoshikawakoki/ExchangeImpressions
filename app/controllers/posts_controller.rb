ac68f1dc8c0158b3458b6e902e1cd7a1a97e76be
yoshikawakoki <cooler970708@gmail.com>
2021-12-24 04:02:55 +0000

    投稿編集機能の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update
+    redirect_to post_path(@post.id)
+  end
+
   def new
     @post = Post.new
   end
