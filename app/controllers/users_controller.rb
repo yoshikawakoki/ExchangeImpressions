850f0265952e82704158f16c71c66e8f564a73d8
yoshikawakoki <cooler970708@gmail.com>
2021-12-24 03:48:57 +0000

    ユーザー一覧表示、編集画面の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
   end
-  
+
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(current_user.id)
+    end
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
 end
