5c3b5ec10972b75e5a53f1afa9dac0889abf3073
yoshikawakoki <cooler970708@gmail.com>
2021-12-28 10:10:54 +0000

    [Fix] Migrations

--- db/migrate/20211217061604_create_post_hashtag_relations.rb
+++ db/migrate/20211217061604_create_post_hashtag_relations.rb
@@ -1,8 +1,8 @@
 class CreatePostHashtagRelations < ActiveRecord::Migration[5.2]
   def change
     create_table :post_hashtag_relations do |t|
-      t.references :post, index: true, foreign_key: true
-      t.references :hashtag, index: true, foreign_key: true
+      t.references :post, foreign_key: true
+      t.references :hashtag, foreign_key: true
 
       t.timestamps
     end
