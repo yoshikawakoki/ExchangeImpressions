109a0bd4dfeaac60416d232d6bba9df4028808da
yoshikawakoki <cooler970708@gmail.com>
2022-01-12 09:19:30 +0000

    画像読み込みのシークレットキー追記

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,5 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+
+Refile.secret_key = '276782903b3988253931be65b80f89cf3900a209a898e40253b6d25af254cbf801d2113208625f49d812dcf4ac9c59f6a04fdab6897a2693440aa6c3084d80bf'
